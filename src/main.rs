152bb76083ebaed63860cf1c9f1112835384e065
stariq <sannantariqa@gmail.com>
2023-10-28 16:34:03 +0200

    [day4] part 1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,26 @@
-use std::collections::HashSet;
 use std::io::{self, BufRead};
 
-fn get_prio(c: char) -> u64 {
-    let val = c as u64;
-    // println!("Val: {}: {}", c, val);
-    if val >= ('a' as u64) {
-        val - ('a' as u64) + 1
-    } else {
-        val - ('A' as u64) + 27
-    }
-}
 fn main() {
-    // println!("{}", 'a' as u64);
-    let mut total_common_prio = 0u64;
-    let mut h: HashSet<char> = HashSet::new();
-    let mut common: HashSet<char> = HashSet::new();
-    let mut remaining_group = 3;
+    let mut count = 0u64;
     for line in io::stdin().lock().lines() {
         let line = line.unwrap();
-        remaining_group -= 1;
 
-        let _ = match remaining_group {
-            2 => {
-                common = HashSet::new();
-                let _ = line
-                    .chars()
-                    .map(|c| common.insert(c))
-                    .collect::<Vec<bool>>();
+        let res: Vec<u64> = line
+            .split(",")
+            .flat_map(|s| s.split("-"))
+            .filter_map(|n| n.parse().ok())
+            .collect();
+
+        assert_eq!(4, res.len());
+
+        let _ = match res[..] {
+            [a, b, x, y] => {
+                if ((a <= x) && (b >= y)) || ((x <= a) && (y >= b)) {
+                    count += 1;
+                }
             }
-            0 => {
-                h.clear();
-                let _ = line.chars().map(|c| h.insert(c)).collect::<Vec<bool>>();
-                common = common.intersection(&h).map(|&c| c).collect();
-                assert_eq!(common.len(), 1);
-                total_common_prio += get_prio(common.drain().next().unwrap());
-                common.clear();
-                remaining_group = 3;
-            }
-            _ => {
-                h.clear();
-                let _ = line.chars().map(|c| h.insert(c)).collect::<Vec<bool>>();
-                common = common.intersection(&h).map(|&c| c).collect();
-            }
+            _ => unreachable!(),
         };
     }
-    println!("Common Prio Total: {}", total_common_prio);
+    println!("Total Covered Assignemnts: {:?}", count);
 }
